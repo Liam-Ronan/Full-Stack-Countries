51a11472dbb368310215e8a2547665d2d6dfb7a7
Liam Ronan <liamronan16@gmail.com>
2023-10-07 10:54:15 +0100

    - imported CORS to handle headers, security - process.env object to access the env variables - created country list to specify what exact countries will be retrieved from the REST countries APi - added new GET request to retrive a country by name

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 // Importing axios to retrieve data from REST countries API
 import axios from 'axios';
+import cors  from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 3000;
@@ -10,7 +11,7 @@ const countriesList = [
     "Albania",
     "Andorra",
     "Australia",
-   "Brazil",
+    "Brazil",
     "Belgium",
     "Canada",
     "China",
@@ -18,7 +19,7 @@ const countriesList = [
     "Germany",
     "India",
     "Indonesia",
-    "Ireland",
+    "Republic Of Ireland",
     "Italy",
     "Japan",
     "Kenya",
@@ -41,8 +42,12 @@ const countriesList = [
 
 // Adding middleware to parse through the JSON data/URL-encoded data
 app.use(express.json());
+// 
+app.use(cors())
+// Handle encoded data - spaces, special chars etc.
 app.use(express.urlencoded({ extended: true }))
 
+// GET request to ensure server is running on port 3000
 app.get('/', (req, res) => {
     try {
         res.send(`Express server running on Port: ${PORT}`)   
@@ -64,28 +69,54 @@ async function getCountryData(countryName) {
         return response.data[0];
     } 
     catch(err) {
-        
+        console.log(`Error fetching data for ${countryName}`);
     }
 }
 
 /* Using async/await to ensure express server stays
-responsive - doesn't block the event loop,
-maintaining scalability of server */
+   responsive - doesn't block the event loop,
+   maintaining scalability of server */
 app.get('/countries', async (req, res) => {
     try {
-        //Getting all countries from the URL - returns a promise
-        const response = await axios.get('https://restcountries.com/v3.1/all')
+        /* - for each country in array
+           - getCountryData is called for each country in the list */
+        const countryData = countriesList.map((countryName) => {
+            return getCountryData(countryName)
+        })
         
-        //Response data stored in countries variable
-        const countries = response.data;
+        // Wait for promises to resolve
+        const countryDataList = await Promise.all(countryData)
+
+        // Filter out countries where data could not be retrieved
+        const validCountryData = countryDataList.filter((data) => data !== null);
 
         //Sends countries data as JSON to client
-        res.json(countries)
+        res.json(validCountryData)
     } 
     catch(err) {
-        console.log(`Error retrieving data: ${err}`);
+        console.log(`Error retrieving data:`, err);
         res.status(500).json( {error: 'Server Error'})
     }
+})
+
+app.get('/countries/:countryName', async (req, res) => {
+    try {
+        const countryName= req.params.countryName;
+
+        const countryData = await getCountryData(countryName)
+
+        if(countryData) {
+            res.json(countryData)
+        }
+        else {
+            res.status(404).json({ error: 'Country not found'})
+        }
+    } 
+    catch(error) {
+        console.log('Error retrieving data', err);
+        res.status(500).json({ error: 'Server error'})
+    }
+
 })
 
 app.listen(PORT, () => {
