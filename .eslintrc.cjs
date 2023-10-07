673ec1c64b93d65289ece3b8590f8d92a63bf3ea
Liam Ronan <liamronan16@gmail.com>
2023-10-07 09:38:54 +0100

    created country list for specified countries added function to recieve this data

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,16 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true,
+         es2020: true,
+         node: true,
+         commonjs: true
+       },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    "eslint-disable-line no-unused-vars"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
--- server.js
+++ server.js
@@ -1,14 +1,90 @@
-import express from "express";
+import express from 'express';
+// Importing axios to retrieve data from REST countries API
+import axios from 'axios';
 
 const app = express()
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
+// List of countries to retrieve data for
+const countriesList = [
+    "Albania",
+    "Andorra",
+    "Australia",
+   "Brazil",
+    "Belgium",
+    "Canada",
+    "China",
+    "France",
+    "Germany",
+    "India",
+    "Indonesia",
+    "Ireland",
+    "Italy",
+    "Japan",
+    "Kenya",
+    "Luxembourg",
+    "Mexico",
+    "New Zealand",
+    "Nigeria",
+    "Portugal",
+    "Russia",
+    "South Africa",
+    "South Korea",
+    "Spain",
+    "Sweden",
+    "Thailand",
+    "United Kingdom",
+    "United States of America",
+    "Vietnam",
+    "Zambia",
+];
+
+// Adding middleware to parse through the JSON data/URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
     try {
         res.send(`Express server running on Port: ${PORT}`)   
     } 
     catch(err) {
         res.status(500).send(err)
+    }
+})
+
+//Async function to retrieve data only for specified countries above
+async function getCountryData(countryName) {
+    try {
+        // Retrieving data for specified country from the API
+        const response = await axios.get(`https://restcountries.com/v3.1/name/${countryName}`)
+       /* Extracting data from HTTP response
+        - React components can consistently
+        - expect data structure to be the same
+        - ease of mapping an array of objects */
+        return response.data[0];
+    } 
+    catch(err) {
+        
+    }
+}
+
+/* Using async/await to ensure express server stays
+responsive - doesn't block the event loop,
+maintaining scalability of server */
+app.get('/countries', async (req, res) => {
+    try {
+        //Getting all countries from the URL - returns a promise
+        const response = await axios.get('https://restcountries.com/v3.1/all')
+        
+        //Response data stored in countries variable
+        const countries = response.data;
+
+        //Sends countries data as JSON to client
+        res.json(countries)
+    } 
+    catch(err) {
+        console.log(`Error retrieving data: ${err}`);
+        res.status(500).json( {error: 'Server Error'})
     }
 })
 
