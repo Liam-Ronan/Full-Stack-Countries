5b5a8d049f17d6b407385503c6fb803f451a429a
Liam Ronan <liamronan16@gmail.com>
2023-11-04 12:36:42 +0000

    merge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { BsGlobeAmericas } from "react-icons/bs";
 import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 export const Header = () => {
   return (
     <>
-        <div className="container-fluid d-flex justify-content-evenly py-5 bg-colour">
+        <Container fluid className="d-flex justify-content-evenly py-5 bg-colour">
               <Link to={'/'} className='text-decoration-none'><h1 className="fw-bolder fs-1 text-light"><BsGlobeAmericas className="mb-2 mx-2"/>WorldWise</h1></Link>
 
               <h3 className="fs-5 text-light mt-3 fw-light">By Liam Ronan</h3>
-          </div>
+          </Container>
     </>
   )
 }
