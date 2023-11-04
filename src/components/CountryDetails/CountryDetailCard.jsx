2af28e292619c3dfad50886856175c0d37f5f328
Liam Ronan <liamronan16@gmail.com>
2023-11-04 21:33:54 +0000

    additional API

--- src/components/CountryDetails/CountryDetailCard.jsx
+++ src/components/CountryDetails/CountryDetailCard.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col, Image } from 'react-bootstrap'
+import axios from 'axios';
+
+const weatherApiKey = import.meta.env.VITE_WEATHER_API_KEY;
+const eventApiKey = import.meta.env.VITE_EVENT_API_KEY;
 
 const CountryDetailCard = ({ countryData }) => {
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [eventData, setEventData] = useState(null);
+
+  useEffect(() => {
+
+    const weatherApiURL = `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${countryData.latlng[0]},${countryData.latlng[1]}&aqi=no`;
+
+    const fetchWeatherData = () => {
+      axios.get(weatherApiURL)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+    }
+
+    fetchWeatherData()
+  }, [countryData.latlng])
+
+  useEffect(() => {
+
+    const eventApiURL = `https://app.ticketmaster.com/discovery/v2/events?countryCode=${countryData.cca2}&${eventApiKey}`
+
+    const fetchEventData = () => {
+      axios.get(eventApiURL)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+    }
+
+    fetchEventData()
+  }, [countryData.cca2])
+
   return (
     <Container fluid className='bg-colour pt-4'>
         <Row className="pt-4 d-flex justify-content-center bg-colour">
@@ -14,6 +56,8 @@ const CountryDetailCard = ({ countryData }) => {
               <p className='mx-5 pt-4 fw-normal'>Sub-Region: {countryData.subregion}</p>
               <p className='mx-5 pt-4 fw-normal'>Capital: {countryData.capital}</p>
               <p className='mx-5 pt-4 fw-normal'>Population: {countryData.population.toLocaleString()}</p>
+              <p className='mx-5 pt-4 fw-normal'>Latitude: {countryData.latlng[0].toFixed(1)}</p>
+              <p className='mx-5 pt-4 fw-normal'>Longitude: {countryData.latlng[1].toFixed(1)}</p>
             </Col>
 
             <Col md={3} sm={8} className="p-4">
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { config } from 'dotenv';
+
+// Load environment variables from .env file
+config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      // Add a custom Replace plugin to replace __VITE_ENV__ with environment variables
+      plugins: [
+        {
+          name: 'replace-vite-env',
+          transform(code, id) {
+            if (id.endsWith('/vite.config.js')) {
+              // Replace __VITE_ENV__ with the JSON stringified environment variables
+              code = code.replace('__VITE_ENV__', JSON.stringify(process.env));
+            }
+            return {
+              code,
+              map: null,
+            };
+          },
+        },
+      ],
+    },
+  },
+});
